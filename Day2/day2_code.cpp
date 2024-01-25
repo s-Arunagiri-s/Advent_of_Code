e13ba08dc08ddbcc73a32f656e8d85770bf04025
s-Arunagiri-s <106005283+s-Arunagiri-s@users.noreply.github.com>
2024-01-25 13:26:48 +0530

    Add files via upload

---
+++ Day2/day2_code.cpp
@@ -0,0 +1,82 @@
+
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<sstream>
+using namespace std;
+int main(){
+    fstream file;
+    int blue;
+    int red;
+    int green;
+    cout<<"enter number of blue cubes:";
+    cin>>blue;
+    cout<<"enter number of red cubes:";
+    cin>>red;
+    cout<<"enter number of green cubes:";
+    cin>>green;
+    file.open("day2.txt",ios::in);
+    if(file.is_open())
+    {
+        string tp;
+        int game;
+        string s;
+        int bp;
+        int rp;
+        int gp;
+        int b=0;
+        int r=0;
+        int g=0;
+        int flag=0;
+        string ss;
+        int sum=0;
+        while(getline(file,tp)){
+            for(int i=0;i<tp.length();i++)
+            {
+                if(tp[i]<='9' && tp[i]>='0')
+                {
+                    game=int(tp[i]-'0');
+                    break;
+                }
+            }
+            s=tp.substr(8,tp.size()-8);
+            //cout<<s<<"\n";
+            stringstream sss(s);
+            while(getline(sss,ss,';'))
+            {
+               b=0;
+               r=0;
+               g=0;
+              // cout<<ss<<"\n";
+               bp=ss.find("blue")-3;
+               rp=ss.find("red")-3;
+               gp=ss.find("green")-3;
+
+               if(ss[bp]!=' ')
+               b=int(ss[bp]-'0')*10;
+               b+=int(ss[bp+1]-'0');
+               //cout<<"b"<<b<<" ";
+
+               if(ss[rp]!=' ')
+               r=int(ss[rp]-'0')*10;
+               r+=int(ss[rp+1]-'0');
+               //cout<<"r"<<r<<" ";
+
+               if(ss[gp]!=' ')
+               g=int(ss[gp]-'0')*10;
+               g+=int(ss[gp+1]-'0');
+               //cout<<"g"<<g<<" ";
+
+               if(b>blue || r>red || g>green)
+               flag=1;
+            }
+          if(flag==0)
+          {
+          cout<<"game:"<<game<<"\n";
+          sum+=game;
+          }
+          flag=0;
+        }
+        cout<<sum;
+    }
+}
