b7adfe34c42084dd66d1a56b88e1205291eb2e90
s-Arunagiri-s <106005283+s-Arunagiri-s@users.noreply.github.com>
2024-02-01 16:17:02 +0530

    Create day3_part2_code.cpp

---
+++ Day3/day3_part2_code.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int numgen(int i,int j,string &text)
+{
+
+    int num=0;
+    while(isdigit(text[j])==1 && j>=0)
+    {
+        j--;
+    }
+
+    j++;
+
+    while(isdigit(text[j]) && j<=text.size())
+    {
+        num+=int(text[j]-'0');
+        num=num*10;
+        text[j]='.';
+        j++;
+    }
+    cout<<num/10<<" ";
+    return num/10;
+}
+
+int main()
+{
+    fstream file;
+    vector<string> text;
+    string tp;
+    int num_parts=0;
+    long int sum=1;
+    long int gear_ratio=0;
+    file.open("day1.txt",ios::in);
+    if(file.is_open())
+    {
+      while(getline(file,tp))
+        {
+         text.push_back(tp);
+        }
+    }
+    for(int i=0;i<text.size();i++)
+    {
+        for(int j=0;j<text[0].size();j++)
+        {
+
+            if(text[i][j]=='*')
+            {
+
+             if(( isdigit(text[i-1][j-1])==1 && ((i-1>=0 && i<text.size()) && (j-1>=0 && j<text[0].size()))))
+             {
+                num_parts++;
+                sum*=numgen(i-1,j-1,text[i-1]);
+             }
+
+             if(( isdigit(text[i-1][j])==1 && ((i-1>=0 && i<text.size()) && (j>=0 && j<text[0].size()))))
+             {
+                num_parts++;
+                sum*=numgen(i-1,j,text[i-1]);
+             }
+             if(( isdigit(text[i-1][j+1])==1 && ((i-1>=0 && i<text.size()) && (j>=0 && j+1<text[0].size()))))
+             {
+                 num_parts++;
+                 sum*=numgen(i-1,j+1,text[i-1]);
+             }
+             if(( isdigit(text[i][j-1])==1 && ((i>=0 && i<text.size()) && (j-1>=0 && j<text[0].size()))))
+             {
+                 num_parts++;
+                 sum*=numgen(i,j-1,text[i]);
+             }
+             if(( isdigit(text[i][j+1])==1 && ((i>=0 && i<text.size()) && (j>=0 && j+1<text[0].size()))))
+             {
+                 num_parts++;
+                 sum*=numgen(i,j+1,text[i]);
+             }
+             if(( isdigit(text[i+1][j-1])==1 && ((i>=0 && i+1<text.size()) && (j-1>=0 && j<text[0].size()))))
+             {
+                 num_parts++;
+                 sum*=numgen(i+1,j-1,text[i+1]);
+             }
+             if(( isdigit(text[i+1][j])==1 && ((i>=0 && i+1<text.size()) && (j>=0 && j<text[0].size()))))
+             {
+                 num_parts++;
+                 sum*=numgen(i+1,j,text[i+1]);
+             }
+             if(( isdigit(text[i+1][j+1])==1 && ((i>=0 && i+1<text.size()) && (j>=0 && j+1<text[0].size()))))
+             {
+                 num_parts++;
+                 sum*=numgen(i+1,j+1,text[i+1]);
+             }
+            }
+            if(num_parts==2)
+            {
+                gear_ratio+=sum;
+            }
+            sum=1;
+            num_parts=0;
+
+        }
+
+    }
+    cout<<"sum: "<<gear_ratio<<"\n";
+
+}
